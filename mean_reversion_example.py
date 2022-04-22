e22a8c932a2cd98d502463c61aef4ea554d77787
Oatesha <73292759+Oatesha@users.noreply.github.com>
2022-04-22 11:50:06 +0800

    Added Graphing for shorts and covers + cleaned up 250SMA + RSI strategy

--- mean_reversion_example.py
+++ mean_reversion_example.py
@@ -24,21 +24,27 @@ def logic(account, lookback): # Logic function to be used for each time interval
     
     today = len(lookback)-1
     if(today > training_period): # If the lookback is long enough to calculate the Bollinger Bands
+        
+        if(lookback['close'][today] < lookback['SMA_250'][today]): #if the current price is below the 250 SMA we look for short positions
+             #we only want to evaluate potential longs if we have capital to do so
+            if(lookback['RSI'][today] > 20 and account.buying_power > 0): #if the RSI is above 20 and as such the stock is not 'oversold' we can look to short
+                account.enter_position('short', (account.buying_power), lookback['close'][today]) #enter a short position
+                lookback['shorts'][today] = lookback['close'][today] #set the short row for this price to the current position and price for graphing
+            elif(lookback['RSI'][today] <= 20): # the rsi of the current price must be below 20 and as such we look to close any shorts we have as the stock is now oversold and highly likely to return to the mean
+                for position in account.positions: # Close all current positions
+                        account.close_position(position, 1, lookback['close'][today])
+                lookback['covers'][today] = lookback['close'][today] #set the covers row for this price to the current position and price for graphing
+                         
+        if(lookback['close'][today] > lookback['SMA_250'][today]): #Else means that the current price must be above the 250 SMA and as such we look for long positions
+            if(lookback['RSI'][today] <= 80 and account.buying_power > 0): # If the RSI is below 80 the stock is not yet 'overbought' and theres potentially more bullish movement we can take advantage on with a long position
+                account.enter_position('long', (account.buying_power), lookback['close'][today])
+                lookback['buys'][today] = lookback['close'][today] #set the long row for this price to the current position and price for graphing
+            elif(lookback['RSI'][today] >= 80):
+                for position in account.positions: # Close all current positions
+                        account.close_position(position, 1, lookback['close'][today])
+                lookback['sells'][today] = lookback['close'][today] #set the covers row for this price to the current position and price for graphing
+                
 
-        if(lookback['RSI'][today] < 30): # If current price is below lower Bollinger Band, enter a long position
-            # for position in account.positions: # Close all current positions
-            #     account.close_position(position, 1, lookback['close'][today])
-
-            if(account.buying_power > 0):
-                account.enter_position('long', account.buying_power, lookback['close'][today]) # Enter a long position
-            lookback["buys"][today] = lookback['close'][today]
-            
-        if(lookback['RSI'][today] > 70): # If today's price is above the upper Bollinger Band, enter a short position
-            for position in account.positions: # Close all current positions
-                account.close_position(position, 1, lookback['close'][today])
-            lookback["sells"][today] = lookback['close'][today]
-            # if(account.buying_power > 0):
-            #     account.enter_position('short', account.buying_power, lookback['close'][today]) # Enter a short position
 
 '''
 preprocess_data() function:
@@ -53,7 +59,7 @@ def preprocess_data(list_of_stocks):
     list_of_stocks_processed = []
     for stock in list_of_stocks:
         df = pd.read_csv("data/" + stock + ".csv", parse_dates=[0])
-        print(df.head)
+        df = df.iloc[::60, :]
         
         df['TP'] = (df['close'] + df['low'] + df['high'])/3 # Calculate Typical Price
         df['std'] = df['TP'].rolling(training_period).std() # Calculate Standard Deviation
@@ -78,6 +84,8 @@ def preprocess_data(list_of_stocks):
         
         df['buys'] = "" # Create a column to store the number of buys
         df['sells'] = "" # Create a column to store the number of sells
+        df['shorts'] = "" # Create a column to store the number of shorts
+        df['covers'] = "" # Create a column to store the number of covers
         
         df['SMA_250'] = df['TP'].rolling(250).mean() # Calculate Moving Average of Typical Price
         df["SMA_25"] = df['TP'].ewm(25).mean() # Calculate Moving Average of Typical Price
@@ -91,16 +99,20 @@ def preprocess_data(list_of_stocks):
 def plot_stocks(df):
     df = pd.read_csv("data/" + stock +'.csv', parse_dates=[0])
     plt.title('Price chart ')
-    # plt.plot(df['date'], df['SMA_250'])
+    plt.plot(df['date'], df['SMA_250'])
+    plt.plot(df['date'], df['RSI'])
     # plt.plot(df['date'], df['SMA_25'])
     plt.scatter(df['date'], df['buys'],c="red")
-    plt.scatter(df["date"], df["sells"],c="purple")
+    plt.scatter(df["date"], df['sells'],c="purple")
+    plt.scatter(df['date'], df['shorts'],c="yellow")
+    plt.scatter(df["date"], df['covers'],c="black")
     plt.plot(df['date'], df['close'])
     plt.show()
 
 if __name__ == "__main__":
-    # list_of_stocks = ["TSLA_2020-03-01_2022-01-20_1min"] 
-    list_of_stocks = ["TSLA_2020-03-09_2022-01-28_15min", "AAPL_2020-03-24_2022-02-12_15min"] # List of stock data csv's to be tested, located in "data/" folder 
+    list_of_stocks = ["TSLA_2020-03-01_2022-01-20_1min", "GME_2020-04-30_2022-03-21_1min"]
+     
+    # list_of_stocks = ["GME_2020-04-19_2022-03-10_1min"] # List of stock data csv's to be tested, located in "data/" folder 
     list_of_stocks_proccessed = preprocess_data(list_of_stocks) # Preprocess the data
     results = tester.test_array(list_of_stocks_proccessed, logic, chart=True) # Run backtest on list of stocks using the logic function
 
